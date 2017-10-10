782a30d69b5f1b616a291d9bf71be6078220e6f0
teesloane <tylersloane@gmail.com>
2017-10-10 10:32:50 -0400

    more packages

--- tees/config.el
+++ tees/config.el
@@ -2,25 +2,29 @@
 
 ;; TODO: avy jump between buffers
 ;; TODO: neotree adjustable or add treemacs.
-;; TODO: add parinfer 
+;; TODO: add parinfer
 ;; TODO: add cmd+z / cmd+shift+z undo redo
+;; TODO: ctrl + tab for moving persp
+;; TODO: add layers to the windows for nested perspectives.
 
-;; PACKAGES 
+;; PACKAGES
 (def-package! rjsx-mode        :demand t :config)
 (def-package! emojify          :demand t :config)
 (def-package! doom-themes      :demand t :config)
 (def-package! prettier-js      :demand t :config)
 (def-package! browse-at-remote :demand t :config)
+(def-package! yaml-mode        :demand t :config)
+(def-package! markdown-mode    :demand t :config)
 
 ;; MODES
 (push '("\\.js\\'" . rjsx-mode) auto-mode-alist)
 
 ;; DEFAULTS
-(setq-default 
+(setq-default
     ;; GENERAL STUFF
     line-spacing 0.8
     tab-width 2
-    which-key-idle-delay 0.3 
+    which-key-idle-delay 0.3
     initial-major-mode 'org-mode      ;; set scratch buffer to org
 
     ;; JAVASCRIPT STUFF
@@ -28,7 +32,7 @@
     js2-highlight-level 3
     js2-basic-offset 2
     js-indent-level 2
-    
+
     ;; PLUGINS
     evil-goggles-duration 0.100 ;; default is 0.200
 )
