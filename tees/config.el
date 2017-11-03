7c123db46081fbd13a8ee7c24425caf5eb9d4e63
teesloane <tylersloane@gmail.com>
2017-11-03 18:14:58 -0400

    unbork

--- tees/config.el
+++ tees/config.el
@@ -1,18 +1,16 @@
-;;; private/tees/config.el -*- lexical-binding: t; -*-
+;;;; private/tees/config.el -*- lexical-binding: t; -*-
 
-;; TODO: add cmd+z / cmd+shift+z undo redo
-
-;; Other / Files to lad
 (load! +bindings)
+(load! +fira)
 
-;; Deterimine my personal packages
+;;;; MODES
 (def-package! flx :demand t)
 (def-package! prettier-js    :mode "\\.js$"           :config)
 (def-package! js-import      :commands js-import      :config)
 (def-package! rjsx-mode      :commands rjsx-mode      :config)
 (def-package! writeroom-mode :commands writeroom-mode :config)
 
-;; Setup modes as necessary (ugh something should change here...)
+;;;; Setup modes as necessary (ugh something should change here...)
 (push '("\\.js\\'"   . rjsx-mode)            auto-mode-alist)
 (push '("\\.css\\'"  . web-mode)             auto-mode-alist)
 (push '("\\.sass\\'" . sass-mode)            auto-mode-alist)
@@ -22,33 +20,31 @@
 
 ;; DEFAULTS
 (setq-default
-    ;; GENERAL STUFF
-    line-spacing 0.8
-    tab-width 2
-    indent-tab-mode nil
-    which-key-idle-delay 0.3
-    initial-major-mode 'org-mode      ;; set scratch buffer to org
+ ;; GENERAL STUFF
+ line-spacing 0.5
+ tab-width 2
+ indent-tab-mode nil
+ which-key-idle-delay 0.3
 
-    ;; JAVASCRIPT STUFF
-    js2-bounce-indent-p nil
-    js2-highlight-level 3
-    js2-basic-offset 2
-    js-indent-level 2
-    web-mode-markup-indent-offset 2
-    web-mode-css-indent-offset 2
-    web-mode-code-indent-offset 2
-    web-mode-style-padding 2
-    web-mode-script-padding 2
+ ;;;; JAVASCRIPT STUFF
+ js2-bounce-indent-p nil
+ js2-highlight-level 3
+ js2-basic-offset 2
+ js-indent-level 2
+ web-mode-markup-indent-offset 2
+ web-mode-css-indent-offset 2
+ web-mode-code-indent-offset 2
+ web-mode-style-padding 2
+ web-mode-script-padding 2
 
-    ;; PLUGINS
-    avy-all-windows t
+ ;; PLUGINS
+ avy-all-windows t
 )
-
 ;; Local Vars?
 (setq ivy-re-builders-alist '((t . ivy--regex-fuzzy))) ;; make ivy fuzzy search
 
 
-;; hooks
+;;;; hooks
 (defun my-web-mode-hook ()
     "Hooks for Web mode."
     (setq web-mode-markup-indent-offset 2)
@@ -59,12 +55,3 @@
 
 (add-hook 'web-mode-hook  'my-web-mode-hook)
 
-;; Edge Cases
-
-;; Don't use shackle popups for clojure cider repl
-(set! :popup "^\\*cider:" :regexp t :align 'right :size 80) ;; not doing anything yet.
-;; setup cider
-(setq cider-cljs-lein-repl
-	"(do (require 'figwheel-sidecar.repl-api)
-         (figwheel-sidecar.repl-api/start-figwheel!)
-         (figwheel-sidecar.repl-api/cljs-repl))")
