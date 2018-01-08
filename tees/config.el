3218cc2121616cda843960743744f7107a46d1c5
teesloane <tylersloane@gmail.com>
2018-01-07 22:33:49 -0500

    fix fix i donno what im doing bye

--- tees/config.el
+++ tees/config.el
@@ -12,13 +12,21 @@
 (def-package! prettier-js    :mode "\\.js$"           :config)
 (def-package! js-import      :commands js-import      :config)
 (def-package! writeroom-mode :commands writeroom-mode :config)
+
 (def-package! drag-stuff :config
   (drag-stuff-define-keys)
   (drag-stuff-global-mode 1))
+
 (def-package! deft :demand t :config
   (setq deft-extensions '("txt" "tex" "org" "md"))
   (setq deft-use-filename-as-title t)
   (setq deft-directory "~/Dropbox/notes/"))
+
+(def-package! avy
+  :commands (avy-goto-char-2 avy-goto-line)
+  :config
+  (setq avy-all-windows t
+        avy-background t))
 
 (push '("\\.js\\'"   . rjsx-mode)   auto-mode-alist)
 (push '("\\.css\\'"  . web-mode)    auto-mode-alist)
@@ -41,6 +49,45 @@
   (interactive "r")
   (align-regexp start end
                 "\\(\\s-*\\)&" 1 1 t))
+
+;; some cool defaults.
+
+(setq-default
+ ;; GENERAL STUFF
+ line-spacing 0.2
+ tab-width 2
+ indent-tab-mode nil
+ which-key-idle-delay 0.3
+
+ ;;;; JAVASCRIPT STUFF
+ js2-bounce-indent-p nil
+ js2-highlight-level 3
+ js2-basic-offset 2
+ js-indent-level 2
+ web-mode-markup-indent-offset 2
+ web-mode-css-indent-offset 2
+ web-mode-code-indent-offset 2
+ web-mode-style-padding 2
+ web-mode-script-padding 2
+
+ ;; PLUGINS
+ ivy-re-builders-alist '((t . ivy--regex-fuzzy))                                      ;; Make ivy a fuzzy searcher.
+ counsel-rg-base-command "rg -i -M 160 --no-heading --line-number --color never %s ." ;; stop rg crashing on long files.
+ company-idle-delay 0.2
+ company-minimum-prefix-length 2
+ neo-window-width 30
+ neo-window-fixed-size nil
+ deft-auto-save-interval 20
+ avy-all-windows 'all-frames
+
+ ;; ORG MODE
+ org-refile-targets (quote (("notes.org" :maxlevel . 1) ("learning.org" :maxlevel . 3)))
+ org-outline-path-complete-in-steps nil ; Refile in a single go
+ org-refile-use-outline-path t          ; Show full paths for refiling
+ org-log-done 'time
+ org-agenda-files '("~/Dropbox/notes" "~/work/toda/notes")
+ org-tags-column 80
+)
 
  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
  ;; MODES + HOOKS + FUNCTIONS ;;
