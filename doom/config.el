59f893ca415da4af469bb11f24cdd11d205c4226
teesloane <tylersloane@gmail.com>
2019-03-27 09:52:03 -0400

    add back funcs.

--- doom/config.el
+++ doom/config.el
@@ -1,34 +1,31 @@
 (setq-default
- avy-all-windows               'all-frames
- doom-font                     (font-spec :family "Iosevka" :size 14 :weight 'regular)
- doom-theme                    'doom-nord
- which-key-idle-delay          0.2
- ;; Web mode things.
- ;; js2-bounce-indent-p           nil
- ;; js2-highlight-level           3
- ;; js2-basic-offset              2
- ;; js-indent-level               2
- ;; web-mode-markup-indent-offset 2
- ;; web-mode-css-indent-offset    2
- ;; web-mode-code-indent-offset   2
- ;; web-mode-style-padding        2
- ;; web-mode-script-padding       2
- ;; css-indent-offset             2
+  avy-all-windows        'all-frames
+  doom-font              (font-spec :family "Iosevka" :size 14 :weight 'regular)
+  doom-theme             'doom-nord
+  which-key-idle-delay   0.2
  )
-
 
 ;; (load! "+org")
 ;; (load! "+bindings")
 
 
-(defun bjm/align-whitespace (start end)
+(defun tees/align-& (start end)
+  "Align columns by ampersand"
+  (interactive "r")
+  (align-regexp start end
+                "\\(\\s-*\\)&" 1 1 t))
+
+(defun tees/align-whitespace (start end)
   "Align columns by whitespace"
   (interactive "r")
   (align-regexp start end
                 "\\(\\s-*\\)\\s-" 1 0 t))
 
-(defun bjm/align-& (start end)
-  "Align columns by ampersand"
-  (interactive "r")
-  (align-regexp start end
-                "\\(\\s-*\\)&" 1 1 t))
+(defun tees/max-buffer ()
+  "Current buffer becomes full width"
+  (interactive)
+  (if (= 1 (length (window-list)))
+      (jump-to-register '_)
+    (progn
+      (window-configuration-to-register '_)
+      (delete-other-windows))))
