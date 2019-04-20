e378763a2e12d8ed9eae7bef24b23ebd35371aa1
teesloane <tylersloane@gmail.com>
2019-04-20 17:11:11 -0400

    writeroom mode.

--- doom/config.el
+++ doom/config.el
@@ -1,6 +1,7 @@
 (setq-default
   avy-all-windows        'all-frames
-  doom-font              (font-spec :family "Iosevka" :size 14 :weight 'regular)
+  ;; doom-font              (font-spec :family "Iosevka" :size 14 :weight 'regular)
+  doom-font              (font-spec :family "iA Writer Duospace" :size 13 :weight 'regular)
   doom-theme             'doom-nord
   which-key-idle-delay   0.2
  )
@@ -29,3 +30,29 @@
     (progn
       (window-configuration-to-register '_)
       (delete-other-windows))))
+
+;; (add-hook 'markdown-mode-hook 'tees/write)
+
+(defun tees/write ()
+  (interactive)
+  (setq buffer-face-mode-face '(:family "iA Writer Duospace" :height 124)) ; set the font
+  (setq writeroom-width 90)                                                ; set width of writeroom mode
+  (set-fill-column 90)                                                     ; set width of fill column (for text wrapping.)
+  (setq-default indent-tabs-mode t)                                        ; use tabs for indentation
+  (setq-default tab-width 1)                                               ; set tab width to 2 FIXME
+  (setq writeroom-mode-line nil)                                          ; don't show the modeline
+  (setq truncate-lines nil)                                                ; wrap lines?
+  (setq line-spacing 5)                                                    ; set line spacing
+  (setq global-hl-line-mode nil)                                           ; Turn off line highlight
+  (setq display-line-numbers nil)                                          ; don't show line numbers
+  (fringe-mode 0)                                                          ; don't show fringe.
+  (buffer-face-mode)                                                       ; ?
+  (linum-mode 0)                                                           ; turn off line numbers (dooum style.)
+  (global-linum-mode 0)                                                    ; turn off line numbers again.
+  (writeroom-mode 1)                                                       ; go into write room mode.
+  (blink-cursor-mode)                                                      ; let's blink that cursor.
+  (visual-line-mode 1)                                                     ; don't know.
+  (run-at-time "1 sec" nil #'turn-off-solaire-mode)                        ; lol basically set timeout b/c a hook somewhere is turning on solaire mode
+  (hl-line-mode 0)                                                         ; I said stop highlighting stuff!
+  (load-theme 'doom-solarized-light)                                       ; make it dark.
+  )
---
+++ doom/packages.el
@@ -0,0 +1,5 @@
+;; -*- no-byte-compile: t; -*-
+;;; ~/Development/dotfiles/doom/packages.el
+
+
+(package! writeroom-mode)
