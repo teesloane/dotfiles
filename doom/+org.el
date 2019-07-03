a787055061f92db9e30b81700bce86ddd4d6de67
teesloane <tylersloane@gmail.com>
2019-07-03 10:36:17 -0400

    .

--- doom/+org.el
+++ doom/+org.el
@@ -1,94 +1,96 @@
 ;;; ~/Development/dotfiles/doom/+org.el -*- lexical-binding: t; -*-
 
 (after! org
-  ;; Org mode mapping
-  ;; (map! :map evil-org-mode-map
-  ;;       :localleader
-  ;;       :desc "Create_Todo" :nve     "o"  #'org-todo
-  ;;       :desc "Schedule"    :nve     "s"  #'org-schedule
-  ;;       :desc "Deadline"    :nve     "d"  #'org-deadline
-  ;;       :desc "Refile"      :nve     "r"  #'org-refile
-  ;;       :desc "Filter"      :nve     "f"  #'org-match-sparse-tree
-  ;;       :desc "Log"         :nve     "l"  #'org-add-note
-  ;;       :desc "Tag          heading" :nve "t" #'org-set-tags-command)
+	;; Org mode mapping
+	;; (map! :map evil-org-mode-map
+	;;       :localleader
+	;;       :desc "Create_Todo" :nve     "o"  #'org-todo
+	;;       :desc "Schedule"    :nve     "s"  #'org-schedule
+	;;       :desc "Deadline"    :nve     "d"  #'org-deadline
+	;;       :desc "Refile"      :nve     "r"  #'org-refile
+	;;       :desc "Filter"      :nve     "f"  #'org-match-sparse-tree
+	;;       :desc "Log"         :nve     "l"  #'org-add-note
+	;;       :desc "Tag          heading" :nve "t" #'org-set-tags-command)
 
-  (org-super-agenda-mode)
-  (setq wiki-path "~/Dropbox/wiki/")
-  (setq agenda-and-refile-targets
-        '(("wiki.org"     :maxlevel . 1)
-          ("todo.org"     :maxlevel . 1)
-          ("someday.org"  :maxlevel . 1)
-          ("calendar.org" :maxlevel . 1)
-          ("projects.org" :maxlevel . 1)))
+	(org-super-agenda-mode)
+	(setq wiki-path "~/Dropbox/wiki/")
+	(setq agenda-and-refile-targets
+				'(("wiki.org"     :maxlevel . 1)
+					("todo.org"     :maxlevel . 1)
+					("someday.org"  :maxlevel . 1)
+					("calendar.org" :maxlevel . 1)
+					("projects.org" :maxlevel . 1)))
 
-  (toggle-truncate-lines)
+	(toggle-truncate-lines)
 
-  (setq
-   org-directory                      wiki-path
-   org-default-notes-file             (concat wiki-path "index.org")
-   ;; org refile things
-   org-refile-targets                 '(("wiki.org" :maxlevel . 1) ("todo.org"  :maxlevel . 1) ("projects.org" :maxlevel . 1) ("someday.org" :maxlevel . 1))
-   org-outline-path-complete-in-steps nil          ; Refile in a single go
-   org-refile-allow-creating-parent-nodes 'confirm
-   org-refile-use-outline-path        'file            ; Show/full/paths for refiling
-   ;;
-   org-agenda-files                   (list wiki-path)
-   org-fontify-whole-heading-line     nil
-   org-tags-column                    80
-   org-startup-truncated              t
-   org-log-into-drawer                t
-   org-ellipsis " ▼ "
-   org-log-done                       t
+	(setq
+	 org-directory                      wiki-path
+	 org-default-notes-file             (concat wiki-path "index.org")
+	 ;; org refile things
+	 org-refile-targets                 '(("wiki.org" :maxlevel . 1) ("todo.org"  :maxlevel . 1) ("projects.org" :maxlevel . 1) ("someday.org" :maxlevel . 1))
+	 org-outline-path-complete-in-steps nil          ; Refile in a single go
+	 org-refile-allow-creating-parent-nodes 'confirm
+	 org-refile-use-outline-path        'file            ; Show/full/paths for refiling
+	 ;;
+	 org-agenda-files                   (list wiki-path)
+	 org-fontify-whole-heading-line     nil
+	 org-tags-column                    80
+	 org-startup-truncated              t
+	 org-log-into-drawer                t
+	 org-ellipsis " ▼ "
+	 org-log-done                       t
 
-   ;; elfeed things
-   elfeed-search-filter "@1-week-ago"
+	 ;; elfeed things
+	 elfeed-search-filter "@1-week-ago"
 
-   )
+	 )
 
-;; -- org agenda -----------------------------------------------------------------------------------------
+	;; -- org agenda -----------------------------------------------------------------------------------------
 
-  (setq
-   org-agenda-span 'day
-   org-agenda-start-day "+0d"
-   org-agenda-use-time-grid nil
-    org-agenda-skip-scheduled-if-done t
-   )
+	(setq
+	 org-agenda-span 'day
+	 org-agenda-start-day "+0d"
+	 org-agenda-use-time-grid nil
+	 org-agenda-skip-scheduled-if-done t
+	 )
 
-  ;; super agenda configuration
-  (setq org-super-agenda-groups
-        '((:name "Today"   :time-grid t :scheduled today :deadline today)
-          (:name "Overdue"    :deadline past)
-          (:name "Due soon"   :deadline future)
-          (:name "Waiting..." :todo "WAITING")
-          (:name "Habits"     :habit t)
-										(:habit t)
-          ))
+	;; super agenda configuration
+	(setq org-super-agenda-groups
+				'(
+					(:name "Today"      :time-grid t :scheduled today :deadline today)
+					(:name "Overdue"    :deadline past)
+					(:name "Important"  :priority "A")
+					(:name "Due soon"   :deadline future)
+					(:name "Waiting..." :todo "WAITING")
+					(:name "Habits"     :habit t)
+					(:habit t)
+					))
 
-(after! org-agenda
-  (org-super-agenda-mode)
-  (map! :map org-agenda-mode-map
-        ;; :localleader
-        :desc "Forward"       :nve   "]"  #'org-agenda-later
-        :desc "Backward"      :nve   "["  #'org-agenda-earlier
-        :desc "Month View"    :nve   "m"  #'org-agenda-month-view
-        :desc "Week View"     :nve   "w"  #'org-agenda-week-view
-        :desc "Day View"      :nve   "d"  #'org-agenda-day-view
-        :desc "Filter by tag" :nve   "f"  #'org-agenda-filter-by-tag
-        )
+	(after! org-agenda
+		(org-super-agenda-mode)
+		(map! :map org-agenda-mode-map
+					;; :localleader
+					:desc "Forward"       :nve   "]"  #'org-agenda-later
+					:desc "Backward"      :nve   "["  #'org-agenda-earlier
+					:desc "Month View"    :nve   "m"  #'org-agenda-month-view
+					:desc "Week View"     :nve   "w"  #'org-agenda-week-view
+					:desc "Day View"      :nve   "d"  #'org-agenda-day-view
+					:desc "Filter by tag" :nve   "f"  #'org-agenda-filter-by-tag
+					)
 
-  (set-popup-rule! "^\\*Org Agenda.*" :slot -1 :size 190  :select t)
-  (after! evil-snipe (push 'org-agenda-mode evil-snipe-disabled-modes))
-  (set-evil-initial-state! 'org-agenda-mode 'normal)
-  )
+		(set-popup-rule! "^\\*Org Agenda.*" :slot -1 :size 190  :select t)
+		(after! evil-snipe (push 'org-agenda-mode evil-snipe-disabled-modes))
+		(set-evil-initial-state! 'org-agenda-mode 'normal)
+		)
 
 
-  ;; ---- CUSTOM CAPTURE TEMPLATES ------------------------------------------------
+	;; ---- CUSTOM CAPTURE TEMPLATES ------------------------------------------------
 
-  (add-to-list 'org-capture-templates
-               '("b" "New Book"
-                 entry  ; type
-                 (file "books.org") ; target
-                 "* %^{Author} - %^{Title}
+	(add-to-list 'org-capture-templates
+							 '("b" "New Book"
+								 entry  ; type
+								 (file "books.org") ; target
+								 "* %^{Author} - %^{Title}
 :PROPERTIES:
 :author: %\\1
 :title: %\\2
@@ -101,12 +103,12 @@
 :rating: 0
 :END:
 "
-                 :prepend t :kill-buffer t))
+								 :prepend t :kill-buffer t))
 
-  (add-to-list 'org-capture-templates '("i" "Inbox" entry (file "inbox.org") "* %u %?\n%i\n" :prepend t :kill-buffer t))
-  (add-to-list 'org-capture-templates '("l" "Log" entry (file+datetree "log.org.gpg") "**** %U %^{Title} %(org-set-tags-command) \n%?" :prepend t))
-  (add-to-list 'org-capture-templates '("t" "Todo" entry (file+datetree "todo.org") "* TODO %?\n%i" :prepend t))
-  )
+	(add-to-list 'org-capture-templates '("i" "Inbox" entry (file "inbox.org") "* %u %?\n%i\n" :prepend t :kill-buffer t))
+	(add-to-list 'org-capture-templates '("l" "Log" entry (file+datetree "log.org.gpg") "**** %U %^{Title} %(org-set-tags-command) \n%?" :prepend t))
+	(add-to-list 'org-capture-templates '("t" "Todo" entry (file+datetree "todo.org") "* TODO %?\n%i" :prepend t))
+	)
 
 
 ;; ;; HOOKS ------------------------
