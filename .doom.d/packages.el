9af949b0c06db03671cc663dc5a51a2064057270
teesloane <tylersloane@gmail.com>
2020-07-31 18:06:09 -0400

    Add: add spray.el and update old hope

--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -1,6 +1,5 @@
-;; Packages
-
 (package! org-super-agenda)
 (package! solaire-mode :disable t)
 (package! pretty-magit :recipe (:local-repo "~/.doom.d/local"))
 (package! focus)
+(package! spray :recipe (:local-repo "~/.doom.d/local/spray.el"))
