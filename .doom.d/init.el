e4dd5a574ca41036e3480db76d1c811f0efbe65e
teesloane <tylersloane@gmail.com>
2022-06-20 06:53:08 -0400

    stuff.

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -126,7 +126,7 @@
        ;; csharp            ; unity, .NET, and mono shenanigans
        data              ; config/data formats
        ;; (dart +flutter)   ; paint ui and not much else
-       elixir            ; erlang done right
+       (elixir +lsp)            ; erlang done right
        elm               ; care for a cup of TEA?
        emacs-lisp        ; drown in parentheses
        ;;erlang            ; an elegant language for a more civilized age
@@ -180,7 +180,7 @@
        sh                ; she sells {ba,z,fi}sh shells on the C xor
        ;;sml
        ;;solidity          ; do you need a blockchain? No.
-       ;;swift             ; who asked for emoji variables?
+       (swift +lsp)             ; who asked for emoji variables?
        ;;terra             ; Earth and Moon in alignment for performance.
        web               ; the tubes
        yaml              ; JSON, but readable
